d7c93e3ed1abb6a28eac92e0fcfb5c2baee24890
mohammad moghadas <zadeh@jumbula.com>
2022-03-23 08:45:53 +0000

    rewrite - task4

--- 24hr/task_4.py
+++ 24hr/task_4.py
@@ -9,7 +9,7 @@ class calculatconsumption:
 
 
 class displayEnergyconsumption(calculatconsumption):
-    def __init__(self, dName = '', pcW = '', nH = '', electricity_rate= 0.10):
+    def __init__(self, dName = '', pcW = '', nH = '', electricity_rate = 0.10):
         self.electricity_rate = electricity_rate
         super().__init__(dName, pcW, nH)
 
@@ -19,30 +19,40 @@ class displayEnergyconsumption(calculatconsumption):
     def calculate_energy_consumption_per_annual(self):
         return (self.electricity_rate * super().energy_consumption_per_day()) * 365
 
-    def manitor_device(self, budget):
+   
+    def printresult(self):
+        print("Device_Name Power_Consumption Daily_Usage Monthly_Running_Cost")
+        print("{} {:.2f} {:.2f} {}".format(self.dname, self.pcw, self.nh, self.calculate_energy_consumption_per_month()))
+
+def get_info():
+    dName = input('enter device name: ')
+    pcW = float(input('enter power consumption in watts: '))
+    nH = float(input('enter daily usage in hours: '))
+    return dName, pcW, nH
+
+def manitor_device(budget):
         devicelist = []
         sum_annualCost = 0
         sum_monthCost = 0
         for i in range(3):
             dName, pcW, nH = get_info()
-            self.dname = dName
-            self.pcw = int(pcW)
-            self.nh = int(nH)
+            ex= displayEnergyconsumption(dName, pcW, nH, 0.10)
             context = {
-                'dName': self.dname,
-                'pcw': self.pcw,
-                'nh': self.nh,
-                'calculate_energy_consumption_per_month': self.calculate_energy_consumption_per_month(),
-                'calculate_energy_consumption_per_annual': self.calculate_energy_consumption_per_annual()
+                'dName': ex.dname,
+                'pcw': ex.pcw,
+                'nh': ex.nh,
+                'calculate_energy_consumption_per_month': ex.calculate_energy_consumption_per_month(),
+                'calculate_energy_consumption_per_annual': ex.calculate_energy_consumption_per_annual()
             }
 
             devicelist.append(context)
-            sum_monthCost += sum_monthCost + self.calculate_energy_consumption_per_month()
-            sum_annualCost += sum_annualCost + self.calculate_energy_consumption_per_annual()
+            sum_monthCost += sum_monthCost + ex.calculate_energy_consumption_per_month()
+            sum_annualCost += sum_annualCost + ex.calculate_energy_consumption_per_annual()
         
         print("Device_Name Power_Consumption Daily_Usage Monthly_Running_Cost Annual_Running_Cost")
         for item in devicelist:
-            print("{} {:.2f} {:.2f} {} {}".format(item['dName'], item['pcw'], item['nh'], item['calculate_energy_consumption_per_month'], item['calculate_energy_consumption_per_annual']))
+            print("{} {:.2f} {:.2f} {:.2f} {:.2f}".format(item['dName'], item['pcw'], item['nh'], 
+                        item['calculate_energy_consumption_per_month'], item['calculate_energy_consumption_per_annual']))
         
         print("your month running cost for these three device is: ", sum_monthCost)
         print("your annual running cost for these three device is:", sum_annualCost)
@@ -55,24 +65,8 @@ class displayEnergyconsumption(calculatconsumption):
             print('your annual running cost for these three devices is lower your budget')
 
 
-
-
-    def printresult(self):
-        print("Device_Name Power_Consumption Daily_Usage Monthly_Running_Cost")
-        print("{} {:.2f} {:.2f} {}".format(self.dname, self.pcw, self.nh, self.calculate_energy_consumption_per_month()))
-
-def get_info():
-    dName = input('enter device name: ')
-    pcW =input('enter power consumption in watts: ')
-    nH =input('enter daily usage in hours: ')
-    return dName, pcW, nH
-
-
-
-    
-
 def main():
-   ex1= displayEnergyconsumption()
    annual_budget = float(input('Please enter your annual budget: '))
-   ex1.manitor_device(annual_budget)
+   manitor_device(annual_budget)
+
 main()
